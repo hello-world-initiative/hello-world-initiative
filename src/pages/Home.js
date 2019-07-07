61d1b02581b8bb8870239d7e307aa97d62a13e19
Phil Z <phil.b.zona@gmail.com>
2019-07-07 18:42:39 -0500

    Add inside-container utility class

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,10 @@ const Home = () => {
   return (
     <>
       <Header text="Hello World Initiative" pageName="home" />
-      <main>
-        <p>Welcome!</p>
+      <main class="container">
+        <section class="inside-container">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, pariatur minus. Non consectetur autem quisquam culpa quo eos repudiandae, sunt voluptate sapiente amet illum ipsam dicta minima itaque consequuntur soluta corporis corrupti labore sint necessitatibus aliquam esse, accusamus, maxime qui! Eveniet qui repudiandae sit illum ea! Exercitationem perspiciatis voluptates nihil minima iure itaque? Aliquam voluptatum similique delectus quas ex quis, provident consequatur iste tempora fuga, id ea, quos quod. Unde consectetur velit fuga sequi, non ex animi libero amet aut aperiam nihil. Adipisci fugit reiciendis recusandae ipsum pariatur commodi corrupti dolores soluta non quos tenetur, ratione culpa voluptatum distinctio eum!</p>
+        </section>
       </main>
     </>
   )
