ac5490bbd581e122efbe27dbc63f5b30db74ef5e
Phil Z <phil.b.zona@gmail.com>
2019-07-07 18:49:54 -0500

    Add inside-container in headers to handle long page titles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ import React from 'react'
 const Header = ({ text, pageName }) => {
   return (
     <header className={`header header-${pageName}`}>
-      <h1 className="header-title">{text}</h1>
+      {/* We use the .inside-container here to handle longer page titles that might go outside the "content area" on smaller screens */}
+      <div className="inside-container">
+        <h1 className="header-title">{text}</h1>
+      </div>
     </header>
   )
 }
