c461ff3570eb393b5fb92f3d1be7bb068a698ca1
Phil Z <phil.b.zona@gmail.com>
2019-07-13 10:21:14 -0500

    Add highlight spans in headings on each page

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,11 +9,17 @@ const About = () => {
       <Header text="About Us" pageName="about" />
       <main>
         <Section color="dark">
-          <Heading postiion="left">Coding education should be free for everyone.</Heading>
+          <Heading postiion="left">
+            Coding education should be free for <span className="heading-highlight">everyone</span>.
+          </Heading>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem temporibus facere doloremque dolorem quisquam iusto labore ut quia molestias qui, eum atque fuga, cumque nesciunt corrupti. Tenetur illo ut, officia consectetur rem ea dolor fuga consequuntur dicta recusandae necessitatibus. Unde esse ipsam, suscipit facere voluptatibus accusamus commodi magni est itaque.</p>
-          <Heading position="right">HWI is community based.</Heading>
+          <Heading position="right">
+            HWI is <span className="heading-highlight">community</span> based.
+          </Heading>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur recusandae dolore culpa. Eligendi odit sint ut architecto similique dicta tempore soluta laborum pariatur numquam obcaecati natus id beatae maxime praesentium dolorum, blanditiis repudiandae minima in molestiae a, dolorem nemo doloribus? Rerum dignissimos totam velit unde quasi, enim error nisi nobis?</p>
-          <Heading position="right">We use free and inexpensive online learning materials.</Heading>
+          <Heading position="right">
+            We use <span className="heading-highlight">free or inexpensive</span> online learning materials.
+          </Heading>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo minima cumque tempore rerum cupiditate, nobis, hic libero quas non repellat, laborum labore sed eaque consectetur aspernatur officiis architecto fuga. Non accusamus molestias atque quia nihil officia voluptates ipsum. Recusandae dicta dolore ut possimus harum nihil molestiae fugiat quidem laudantium consequatur!</p>
         </Section>
         <Section color="light">
--- src/pages/Community.js
+++ src/pages/Community.js
@@ -9,7 +9,9 @@ const Community = () => {
       <Header text="Community" pageName="community" />
       <main>
         <Section color="dark">
-          <Heading position="left">People learn better together.</Heading>
+          <Heading position="left">
+            People learn better <span className="heading-highlight">together</span>.
+          </Heading>
           <p>This is the top section. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum veritatis labore nam architecto, illo dolores. Optio voluptas similique non libero quia, vel illum aspernatur laudantium delectus, dignissimos animi doloremque, sequi sit corrupti nostrum temporibus repellat ratione ipsa tempore! Debitis iusto incidunt dolorum est quod veniam nisi ipsa molestiae error excepturi, hic, dolores rerum autem amet officia ullam in minima quibusdam eaque magnam quam provident saepe sed quo! Ex error sapiente beatae laboriosam dicta magni magnam. Quisquam mollitia odit expedita consequuntur.</p>
         </Section>
         <Section color="light">
