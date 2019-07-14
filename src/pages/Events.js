bbc4775a0eb93f3674b8b27a13b4f8209319ee29
Phil Z <phil.b.zona@gmail.com>
2019-07-14 15:24:22 -0500

    Added linkbuttons to each page

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Header from '../components/Header'
 import Section from '../components/Section'
 import Heading, {AlternateHeading} from '../components/Heading'
+import LinkButton from '../components/LinkButton'
+
+const meetupURL = "https://www.meetup.com/thehwi"
 
 const Events = () => {
   return (
@@ -16,7 +19,14 @@ const Events = () => {
         </Section>
         <Section color="light">
           <AlternateHeading>Join us at an event</AlternateHeading>
-          <p>This is the bottom section. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum veritatis labore nam architecto, illo dolores. Optio voluptas similique non libero quia, vel illum aspernatur laudantium delectus, dignissimos animi doloremque, sequi sit corrupti nostrum temporibus repellat ratione ipsa tempore! Debitis iusto incidunt dolorum est quod veniam nisi ipsa molestiae error excepturi, hic, dolores rerum autem amet officia ullam in minima quibusdam eaque magnam quam provident saepe sed quo! Ex error sapiente beatae laboriosam dicta magni magnam. Quisquam mollitia odit expedita consequuntur.</p>
+          <div className="linkButton-container">
+            <LinkButton
+              to={meetupURL}
+              text="Meetup"
+              icon={["fab", "meetup"]}
+              type="regular"
+            />
+          </div>
         </Section>
       </main>
     </>
