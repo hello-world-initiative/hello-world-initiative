300cd0c5e3faabddc2dde008c0726852c538320c
Andie Zona <andielzona@gmail.com>
2019-07-27 21:07:52 +0000

    adding styles to link button

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import Section from '../components/Section'
-import Heading, {AlternateHeading} from '../components/Heading'
+import Heading, { AlternateHeading } from '../components/Heading'
 import LinkButton from '../components/LinkButton'
 import Article from '../components/Article'
 
@@ -9,30 +9,29 @@ const meetupURL = "https://www.meetup.com/thehwi"
 
 const Events = () => {
   return (
-    <>
-      <Header text="Events" pageName="events" />
-      <main>
-        <Section color="dark">
+    < >
+    <Header text="Events" pageName="events" /> <
+    main >
+    <Section color="dark">
           <Article>
             <Heading position="left">
               Come <span className="heading-highlight">meet up</span> with others who are learning to code.
             </Heading>
             <p>This is the top section. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum veritatis labore nam architecto, illo dolores. Optio voluptas similique non libero quia, vel illum aspernatur laudantium delectus, dignissimos animi doloremque, sequi sit corrupti nostrum temporibus repellat ratione ipsa tempore! Debitis iusto incidunt dolorum est quod veniam nisi ipsa molestiae error excepturi, hic, dolores rerum autem amet officia ullam in minima quibusdam eaque magnam quam provident saepe sed quo! Ex error sapiente beatae laboriosam dicta magni magnam. Quisquam mollitia odit expedita consequuntur.</p>
           </Article>
-        </Section>
-        <Section color="light">
-          <AlternateHeading position="center">Join us at an event</AlternateHeading>
-          <div className="linkButton-container">
-            <LinkButton
+        </Section> <
+    Section color = "light" >
+    <AlternateHeading position="center">Join us at an event</AlternateHeading> <
+    div className = "linkButton-container" >
+    <LinkButton
               to={meetupURL}
               text="Meetup"
               icon={["fab", "meetup"]}
-              type="regular"
-            />
-          </div>
-        </Section>
-      </main>
-    </>
+              type="solid"
+            /> <
+    /div> < /
+    Section > <
+    /main> < / >
   )
 }
 
