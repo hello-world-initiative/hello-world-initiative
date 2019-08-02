c70950f0218eae72d4324bc887f37abb20b0bb02
Phil Z <phil.b.zona@gmail.com>
2019-08-01 19:38:52 -0500

    Create page data file

---
+++ src/data/pages.js
@@ -0,0 +1,91 @@
+const pageData = {
+  home: {
+    heading: 'Our Mission',
+    body: `
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+    pariatur minus. Non consectetur autem quisquam culpa quo eos
+    repudiandae, sunt voluptate sapiente amet illum ipsam dicta minima
+    itaque consequuntur soluta corporis corrupti labore sint
+    necessitatibus aliquam esse, accusamus, maxime qui! Eveniet qui
+    repudiandae sit illum ea! Exercitationem perspiciatis voluptates
+    nihil minima iure itaque? Aliquam voluptatum similique delectus
+    quas ex quis, provident consequatur iste tempora fuga, id ea, quos
+    quod. Unde consectetur velit fuga sequi, non ex animi libero amet
+    aut aperiam nihil. Adipisci fugit reiciendis recusandae ipsum
+    pariatur commodi corrupti dolores soluta non quos tenetur, ratione
+    culpa voluptatum distinctio eum!
+    `
+  },
+  about: {
+    main: {
+      headings: [
+        'Coding education should be free for everyone',
+        'HWI is community based',
+        'We use free or inexpensive online materials'
+      ],
+      sections: [
+        `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem
+        temporibus facere doloremque dolorem quisquam iusto labore ut quia
+        molestias qui, eum atque fuga, cumque nesciunt corrupti. Tenetur
+        illo ut, officia consectetur rem ea dolor fuga consequuntur dicta
+        recusandae necessitatibus. Unde esse ipsam, suscipit facere
+        voluptatibus accusamus commodi magni est itaque.`,
+        `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Consectetur recusandae dolore culpa. Eligendi odit sint ut
+        architecto similique dicta tempore soluta laborum pariatur numquam
+        obcaecati natus id beatae maxime praesentium dolorum, blanditiis
+        repudiandae minima in molestiae a, dolorem nemo doloribus? Rerum
+        dignissimos totam velit unde quasi, enim error nisi nobis?`,
+        `Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Explicabo minima cumque tempore rerum cupiditate, nobis, hic
+        libero quas non repellat, laborum labore sed eaque consectetur
+        aspernatur officiis architecto fuga. Non accusamus molestias atque
+        quia nihil officia voluptates ipsum. Recusandae dicta dolore ut
+        possimus harum nihil molestiae fugiat quidem laudantium
+        consequatur!`
+      ]
+    },
+    bottom: {
+      heading: 'Want to get involved?',
+      body: `We have an active online community where you can come to get help, chat about coding, or contribute to projects. We also plan regular events for members to work together and learn in person.`
+    }
+  },
+  community: {
+    main: {
+      heading: 'People learn better together',
+      body: `This is the top section. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Ipsum veritatis labore nam architecto, illo
+      dolores. Optio voluptas similique non libero quia, vel illum
+      aspernatur laudantium delectus, dignissimos animi doloremque,
+      sequi sit corrupti nostrum temporibus repellat ratione ipsa
+      tempore! Debitis iusto incidunt dolorum est quod veniam nisi ipsa
+      molestiae error excepturi, hic, dolores rerum autem amet officia
+      ullam in minima quibusdam eaque magnam quam provident saepe sed
+      quo! Ex error sapiente beatae laboriosam dicta magni magnam.
+      Quisquam mollitia odit expedita consequuntur.`
+    },
+    bottom: {
+      heading: 'Join our community'
+    }
+  },
+  events: {
+    main: {
+      heading: 'Come meet up with others who are learning to code',
+      body: `This is the top section. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Ipsum veritatis labore nam architecto, illo
+      dolores. Optio voluptas similique non libero quia, vel illum
+      aspernatur laudantium delectus, dignissimos animi doloremque,
+      sequi sit corrupti nostrum temporibus repellat ratione ipsa
+      tempore! Debitis iusto incidunt dolorum est quod veniam nisi ipsa
+      molestiae error excepturi, hic, dolores rerum autem amet officia
+      ullam in minima quibusdam eaque magnam quam provident saepe sed
+      quo! Ex error sapiente beatae laboriosam dicta magni magnam.
+      Quisquam mollitia odit expedita consequuntur.`
+    },
+    bottom: {
+      heading: 'Join us at an event'
+    }
+  }
+};
+
+export default pageData;
