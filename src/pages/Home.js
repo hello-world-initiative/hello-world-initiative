7273a85c7565e1d2768b371acc6ce933b2096139
Phil Z <phil.b.zona@gmail.com>
2019-08-01 19:48:06 -0500

    Pull home page content from page data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,9 @@ import Section from '../components/Section';
 import Heading from '../components/Heading';
 import Article from '../components/Article';
 
+import pageData from '../data/pages';
+const { home } = pageData;
+
 const Home = () => {
   return (
     <>
@@ -11,21 +14,8 @@ const Home = () => {
       <main className="container">
         <Section color="dark">
           <Article>
-            <Heading position="left">Our mission</Heading>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
-              pariatur minus. Non consectetur autem quisquam culpa quo eos
-              repudiandae, sunt voluptate sapiente amet illum ipsam dicta minima
-              itaque consequuntur soluta corporis corrupti labore sint
-              necessitatibus aliquam esse, accusamus, maxime qui! Eveniet qui
-              repudiandae sit illum ea! Exercitationem perspiciatis voluptates
-              nihil minima iure itaque? Aliquam voluptatum similique delectus
-              quas ex quis, provident consequatur iste tempora fuga, id ea, quos
-              quod. Unde consectetur velit fuga sequi, non ex animi libero amet
-              aut aperiam nihil. Adipisci fugit reiciendis recusandae ipsum
-              pariatur commodi corrupti dolores soluta non quos tenetur, ratione
-              culpa voluptatum distinctio eum!
-            </p>
+            <Heading position="left">{home.heading}</Heading>
+            <p>{home.body}</p>
           </Article>
         </Section>
       </main>
