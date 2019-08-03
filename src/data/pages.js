48985780e69e23a95c7640e8a30758197cfbdd39
Phil Z <phil.b.zona@gmail.com>
2019-08-03 09:56:45 -0500

    Content first draft

--- src/data/pages.js
+++ src/data/pages.js
@@ -1,20 +1,20 @@
 const pageData = {
   home: {
     heading: 'Our Mission',
-    body: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, pariatur minus. Non consectetur autem quisquam culpa quo eos repudiandae, sunt voluptate sapiente amet illum ipsam dicta minima itaque consequuntur soluta corporis corrupti labore sint necessitatibus aliquam esse, accusamus, maxime qui! Eveniet qui repudiandae sit illum ea! Exercitationem perspiciatis voluptates nihil minima iure itaque? Aliquam voluptatum similique delectus quas ex quis, provident consequatur iste tempora fuga, id ea, quos quod. Unde consectetur velit fuga sequi, non ex animi libero amet aut aperiam nihil. Adipisci fugit reiciendis recusandae ipsum pariatur commodi corrupti dolores soluta non quos tenetur, ratione culpa voluptatum distinctio eum!`
+    body: `The mission of the Hello World Initiative is to create an inclusive, welcoming, productive environment for anyone who wants to learn to code. We understand that traditional educational paths aren’t for everyone, and we’re building a community for those who don’t have the means to attend college or a bootcamp. As a group of largely self-taught coders, we’ve collectively spent years discovering what works and what doesn’t, and we’re dedicated to helping others find their own path into the tech industry. We believe that anyone who wants to learn should have access to the resources they need. Whether you’ve never written a line of code in your life or you’re a longtime industry professional, we want to help you meet your goals.`
   },
   about: {
     main: {
       headings: [
-        'Coding education should be free for everyone',
+        'Coding education is for everyone',
         'HWI is community based',
-        'We use free or inexpensive online materials'
+        'We use affordable online learning materials'
       ],
-      highlights: ['everyone', 'community', 'free or inexpensive'],
+      highlights: ['everyone', 'community', 'affordable'],
       sections: [
-        `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem temporibus facere doloremque dolorem quisquam iusto labore ut quia molestias qui, eum atque fuga, cumque nesciunt corrupti. Tenetur illo ut, officia consectetur rem ea dolor fuga consequuntur dicta recusandae necessitatibus. Unde esse ipsam, suscipit facere voluptatibus accusamus commodi magni est itaque.`,
-        `Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur recusandae dolore culpa. Eligendi odit sint ut architecto similique dicta tempore soluta laborum pariatur numquamobcaecati natus id beatae maxime praesentium dolorum, blanditiis repudiandae minima in molestiae a, dolorem nemo doloribus? Rerum dignissimos totam velit unde quasi, enim error nisi nobis?`,
-        `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo minima cumque tempore rerum cupiditate, nobis, hic libero quas non repellat, laborum labore sed eaque consectetur aspernatur officiis architecto fuga. Non accusamus molestias atque quia nihil officia voluptates ipsum. Recusandae dicta dolore ut possimus harum nihil molestiae fugiat quidem laudantium consequatur!`
+        `We believe that no matter who you are and what resources you have available, you can learn to code. The tech industry needs people from a variety of backgrounds and walks of life, and our goal is to make sure people feel welcome and included while they learn to code. Have a question that you’re embarrassed to ask? We won’t judge. Did you make something that doesn’t feel “good enough”? We believe small wins are just as important as big ones. The tech industry needs people like you, and so do we.`,
+        `The Hello World Initiative is not a bootcamp or a company trying to push a certain product - it’s a community. We’re a group of people who meet to support each other, help each other learn, and create projects that foster learning. We think that having a supportive community is one of the most important aspects of learning to code, so we created a space where people can ask for help, find partners to work on projects, and answer questions to solidify their understanding of the topics they’re working on.`,
+        `Whenever possible, we recommend free and open source courses and learning materials. When we use paid resources, generally the cost will be well under $50, and we’ll do our best to provide a free alternative as well. Any cost for resources is totally optional and goes to the creators - we do this because we want to help, not to make money. College and bootcamps are huge investments - we want to ensure that everyone who has a desire to learn about technology has access to the resources they need. Talent is evenly distributed, but opportunity is not. We believe it’s possible to change that.`
       ]
     }
   },
@@ -22,7 +22,7 @@ const pageData = {
     main: {
       heading: 'People learn better together',
       highlight: 'together',
-      body: `This is the top section. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum veritatis labore nam architecto, illo dolores. Optio voluptas similique non libero quia, vel illum aspernatur laudantium delectus, dignissimos animi doloremque, sequi sit corrupti nostrum temporibus repellat ratione ipsa tempore! Debitis iusto incidunt dolorum est quod veniam nisi ipsa molestiae error excepturi, hic, dolores rerum autem amet officia ullam in minima quibusdam eaque magnam quam provident saepe sed quo! Ex error sapiente beatae laboriosam dicta magni magnam. Quisquam mollitia odit expedita consequuntur.`
+      body: `Learning to code can be isolating - it’s easy to feel like you’re not getting it and that tech just isn’t for you. That’s why we created this community. We think that learning coding - or anything else - is easier and more fun when you do it with other people. Our Discord channel is a place for people to ask questions, help each other, and talk about their progress through whatever curriculum they’ve chosen. Most of all, it’s a place where you never have to worry about being judged for not knowing the answer. We were all beginners once, and we believe everyone has something valuable to contribute.`
     },
     bottom: {
       heading: 'Join our community'
@@ -31,8 +31,8 @@ const pageData = {
   events: {
     main: {
       heading: 'Come meet up with others who are learning to code',
-      highlight: 'meet up with others',
-      body: `This is the top section. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum veritatis labore nam architecto, illo dolores. Optio voluptas similique non libero quia, vel illum aspernatur laudantium delectus, dignissimos animi doloremque, sequi sit corrupti nostrum temporibus repellat ratione ipsa tempore! Debitis iusto incidunt dolorum est quod veniam nisi ipsa molestiae error excepturi, hic, dolores rerum autem amet officia ullam in minima quibusdam eaque magnam quam provident saepe sed quo! Ex error sapiente beatae laboriosam dicta magni magnam. Quisquam mollitia odit expedita consequuntur.`
+      highlight: 'meet up',
+      body: `Because we’re focused on building a community, we host in person meetups about once a week so that people can come and work together, learn new skills, and meet others like themselves. Our events are pretty casual - all you need is your computer and a positive mindset. These weekly meetups are held at a coffee shop in the Richardson, TX area, although we are working on more structured events in the future.`
     },
     bottom: {
       heading: 'Join us at an event'
